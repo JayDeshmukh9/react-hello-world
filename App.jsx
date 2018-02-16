5f7a1a020c99a47167667ebbff4526a30929eb8f
JayDeshmukh <jaydeshmukh8551@gmail.com>
2018-02-16 12:26:58 +0530

    youtube api

--- App.jsx
+++ App.jsx
@@ -1,10 +1,46 @@
 import React from 'react';
+import YTSearch from 'youtube-api-search';
+import SearchBar from './src/search_bar.jsx';
+import VideoList from './src/video-list.jsx';
+import VideDetail from './src/video-detail.jsx';
+
+const API_KEY = 'AIzaSyARzlnpzKnvuMPOJiIVDIVg6hkILgjim8E' 	
 
 class App extends React.Component {
+   
+   constructor(props) {
+   	super(props);
+   	this.state={  
+   		videos : [],
+   		selectedVideo:null,
+   	};
+      this.videoSearch("tony stark");
+   }
+
+   videoSearch(term) {
+      YTSearch({key:API_KEY,term:term},(videos) => {
+         this.setState({
+            videos:videos,
+            selectedVideo:videos[2]
+         });
+      });
+   }
+
    render() {
       return (
+         
          <div>
-            Hello Universe!!!
+            <SearchBar onSearchTermChange = {
+                  term => this.videoSearch(term)
+               }
+            />
+            <VideDetail video={this.state.selectedVideo} />
+            <VideoList 
+            	onVideoSelect={
+                  selectedVideo => this.setState({selectedVideo})
+               }
+            	videos={this.state.videos}
+            />
          </div>
       );
    }
---
+++ src/video-list.jsx
@@ -0,0 +1,25 @@
+import React from 'react';
+import VideoListItem from './video-list-item.jsx'
+
+const VideoList = (props) => { 
+
+	const videoItems = props.videos.map((video) => {
+		return (
+			<VideoListItem 
+
+				onVideoSelect={props.onVideoSelect }
+				key={video.etag} 
+				video={video}
+			/>
+		);
+	});
+	return (
+		<ul className="col-md-6 list-group center">
+			{videoItems}
+		</ul>
+	);
+}
+
+
+
+export default VideoList;
