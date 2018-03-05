143405b1e9f854dcd6bc4518c9ab2c758d28b169
Jay Deshmukh <35005482+JayDeshmukh9@users.noreply.github.com>
2018-03-05 11:32:02 +0530

    Update App.jsx

--- App.jsx
+++ App.jsx
@@ -4,7 +4,7 @@ import SearchBar from './src/search_bar.jsx';
 import VideoList from './src/video-list.jsx';
 import VideDetail from './src/video-detail.jsx';
 
-const API_KEY = '#' 	
+const API_KEY = 'process.enc.YTKEY' 	
 
 class App extends React.Component {
    
